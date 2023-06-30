4d1816eb760237bd35ebdabf5f37473d6ad65742
Ventsislav Nikolov <34811964+ventsislavnikolov@users.noreply.github.com>
2023-07-01 01:03:19 +0300

    test(release): fix test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,12 +1,16 @@
-import renderer from 'react-test-renderer';
+import { render, waitFor } from '@testing-library/react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
-import App from '../App';
+import App from '../App'; // Replace with the path to your component
 
-jest.useFakeTimers();
+jest.mock('expo-splash-screen', () => ({
+  hideAsync: jest.fn(),
+  preventAutoHideAsync: jest.fn(), // If you use this method, you should also mock it
+}));
 
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
+describe('App', () => {
+  it('shows the splash screen', async () => {
+    render(<App />);
+    await waitFor(() => expect(SplashScreen.preventAutoHideAsync).toHaveBeenCalled());
   });
 });
