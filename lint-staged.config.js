970cf1cc1bfbcbda7d39b0aa26b98f3975e13ee9
Ventsislav Nikolov <34811964+ventsislavnikolov@users.noreply.github.com>
2024-01-11 10:09:21 +0200

    chore(release): fix dependabot, build and pipeline

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
-  'src/**/*.{css,scss,md}': ['yarn prettier:fix'],
-  'src/**/*.{js,jsx,ts,tsx}': ['yarn lint:fix', 'yarn prettier:fix'],
+  'src/**/*.{css,scss,md}': ['yarn fix:format'],
+  'src/**/*.{js,jsx,ts,tsx}': ['yarn lint', 'yarn fix:format'],
 };
