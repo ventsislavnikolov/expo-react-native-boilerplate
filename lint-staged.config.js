424f65659ebdb729e5dc4b513274884cdf862f3a
Ventsislav Nikolov <34811964+ventsislavnikolov@users.noreply.github.com>
2024-05-17 10:14:04 +0300

    chore: update packages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  './src/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': ['yarn lint', 'yarn prettier'],
-  './__tests__/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': ['yarn lint', 'yarn prettier'],
+  './src/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': ['yarn lint', 'yarn format'],
+  './__tests__/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': ['yarn lint', 'yarn format'],
 };
